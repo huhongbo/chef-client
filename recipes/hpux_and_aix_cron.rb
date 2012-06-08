0675b9882655e36c9b6cc7cb29aca066146fb3ab
dn365 <duning3650@gmail.com>
2012-06-08 11:26:44 +0800

    repair cron script

--- recipes/hpux_and_aix_cron.rb
+++ recipes/hpux_and_aix_cron.rb
@@ -1,8 +1,5 @@
 
-if ::File.executable?(node["chef_client"]["bin"])
-  client_bin = node["chef_client"]["bin"]
-  # search for the bin in some sane paths
-elsif (chef_in_sane_path=Chef::Client::SANE_PATHS.map{|p| p="#{p}/chef-client";p if ::File.executable?(p)}.compact.first) && chef_in_sane_path
+if (chef_in_sane_path=Chef::Client::SANE_PATHS.map{|p| p="#{p}/chef-client";p if ::File.executable?(p)}.compact.first) && chef_in_sane_path
   client_bin = chef_in_sane_path
   # last ditch search for a bin in PATH
 elsif (chef_in_path=%x{which chef-client}.chomp) && ::File.executable?(chef_in_path)
