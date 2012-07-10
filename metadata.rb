6591e80ad8acca4df4dd00d7b77b1a11dc2c8331
dn365 <duning3650@gmail.com>
2012-07-10 16:57:38 +0800

    修正

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.3"
 
 # https://github.com/dn365/chef-base-config
-depends "chef-base-config"
+#depends "chef-base-config"
 
 %w{ aix ubuntu debian redhat centos fedora freebsd  windows hpux }.each do |os|
   supports os
